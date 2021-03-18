46aa97f3c8febf8e0b61d0e9f5514fe49c6c0b9c
Santa <namns.dev@gmail.com>
2021-03-18 10:16:53 +0700

    cleanup

--- btbinance/store.py
+++ btbinance/store.py
@@ -18,33 +18,6 @@ logger = logging.getLogger('BTBinanceStore')
 
 class BinanceStore(CCXTStore):
     '''API provider for Binance feed and broker classes.'''
-
-    # Supported granularities
-    _INTERVALS = {
-        (bt.TimeFrame.Minutes, 1): '1m',
-        (bt.TimeFrame.Minutes, 3): '3m',
-        (bt.TimeFrame.Minutes, 5): '5m',
-        (bt.TimeFrame.Minutes, 15): '15m',
-        (bt.TimeFrame.Minutes, 30): '30m',
-        (bt.TimeFrame.Minutes, 60): '1h',
-        (bt.TimeFrame.Minutes, 90): '90m',
-        (bt.TimeFrame.Minutes, 120): '2h',
-        (bt.TimeFrame.Minutes, 180): '3h',
-        (bt.TimeFrame.Minutes, 240): '4h',
-        (bt.TimeFrame.Minutes, 360): '6h',
-        (bt.TimeFrame.Minutes, 480): '8h',
-        (bt.TimeFrame.Minutes, 720): '12h',
-        (bt.TimeFrame.Days, 1): '1d',
-        (bt.TimeFrame.Days, 3): '3d',
-        (bt.TimeFrame.Weeks, 1): '1w',
-        (bt.TimeFrame.Weeks, 2): '2w',
-        (bt.TimeFrame.Months, 1): '1M',
-        (bt.TimeFrame.Months, 3): '3M',
-        (bt.TimeFrame.Months, 6): '6M',
-        (bt.TimeFrame.Years, 1): '1y',
-    }
-    _INTERVALS_REVERSED = {v: k for k, v in _INTERVALS.items()}
-
     def __init__(self, currency='USDT', sandbox=False, retries=10, **kwargs):
         super().__init__(exchange='binance',
                          currency=currency,
@@ -75,22 +48,9 @@ class BinanceStore(CCXTStore):
 
         self._loop_stream()
 
-    def from_interval(self, interval):
-        timeframe = self._INTERVALS_REVERSED.get(interval)
-        if timeframe is None:
-            raise RuntimeError(f"Interval {interval} is not support")
-        return timeframe
-
-    def to_interval(self, timeframe, compression):
-        interval = self._INTERVALS.get((timeframe, compression))
-        if interval is None:
-            raise RuntimeError(
-                f"Interval for {timeframe, compression} is not support")
-        return interval
-
     # subscribe
     # account
-    def subscribe_account(self, **kwargs):
+    def subscribe_my_account(self, **kwargs):
         return self.subscribe(['arr'], ['!userData'],
                               ['ACCOUNT_UPDATE', 'ORDER_TRADE_UPDATE'],
                               **kwargs)
